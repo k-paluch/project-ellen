fd86163df6517d423f9e5c74b85ae7ca84dba521
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-11-07 15:59:04 +0100

    Update Usable.java

--- src/main/java/sk/tuke/kpi/oop/game/tools/Usable.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/Usable.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package sk.tuke.kpi.oop.game.tools;
 
 import sk.tuke.kpi.gamelib.Actor;
@@ -6,9 +6,3 @@ import sk.tuke.kpi.gamelib.Actor;
 public interface Usable {
     void useWith(Actor actor);
 }
-=======
-package sk.tuke.kpi.oop.game.tools;
-
-public class Usable {
-}
->>>>>>> 653695e3498e9624882463b4c8919ee60c878e78
