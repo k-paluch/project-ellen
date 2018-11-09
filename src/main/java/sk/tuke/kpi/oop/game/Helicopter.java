060f40eb6f333f75911505f74ae01ce51233b0f1
Kristian Paluch <kristian.paluch@student.tuke.sk>
2018-11-09 10:37:09 +0100

    heli

--- src/main/java/sk/tuke/kpi/oop/game/Helicopter.java
+++ src/main/java/sk/tuke/kpi/oop/game/Helicopter.java
@@ -4,49 +4,50 @@ import org.jetbrains.annotations.NotNull;
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.framework.Player;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.gamelib.actions.Invoke;
 
 public class Helicopter extends AbstractActor {
     private boolean follow;
     private Player player;
-    private Animation heli = new Animation("srites/heli.png",64,64,10);
-    public Helicopter(){
+    private Animation heli = new Animation("srites/heli.png", 64, 64, 10);
+
+    public Helicopter() {
         setAnimation(heli);
         follow = false;
     }
 
-    public void searchAndDestroy(){
-        follow  = true;
+    public void searchAndDestroy() {
+        follow = true;
     }
 
-    /*
-    @Override
-    public void addedToScene(@NotNull Scene scene) {
+    public void addedToScene(Scene scene, Invoke invoke, Reactor defLight) {
+        new Loop<>(new Invoke(this::kill)).scheduleOn(this);
+    }
+
+
+    private void kill() {
         int energy = player.getEnergy();
-        if(follow==true) {
-            if(player.getPosY() > getPosY()){
-                int y = getPosY()+1;
-                setPosition(getPosX() , y);
-            }
-            else if(player.getPosY() < getPosY()){
-                int y = getPosY()-1;
-                setPosition(getPosX() , y);
+        if (follow) {
+            if (player.getPosY() > getPosY()) {
+                int y = getPosY() + 1;
+                setPosition(getPosX(), y);
+            } else if (player.getPosY() < getPosY()) {
+                int y = getPosY() - 1;
+                setPosition(getPosX(), y);
             }
 
-            if(player.getPosX() > getPosX()){
-                int x = getPosX()+1;
-                setPosition(x , getPosY());
+            if (player.getPosX() > getPosX()) {
+                int x = getPosX() + 1;
+                setPosition(x, getPosY());
+            } else if (player.getPosX() < getPosX()) {
+                int x = getPosX() - 1;
+                setPosition(x, getPosY());
             }
-            else if(player.getPosX() < getPosX()){
-                int x = getPosX()-1;
-                setPosition(x , getPosY());
-            }
-            if(player.getPosX() == getPosX() && player.getPosY() == getPosY() ){
-                energy--;
-                player.setEnergy(energy);
+            if (player.getPosX() == getPosX() && player.getPosY() == getPosY()) {
+                player.setEnergy(energy--);
             }
         }
-        super.addedToScene(scene);
-    }*/
+    }
 }
