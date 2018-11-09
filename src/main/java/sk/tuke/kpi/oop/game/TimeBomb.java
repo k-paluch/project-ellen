22b045f44eff00597068cc9255dc57241a54138f
Kristian Paluch <kristian.paluch@student.tuke.sk>
2018-11-09 09:46:42 +0100

    explosion

--- src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
@@ -1,4 +1,48 @@
 package sk.tuke.kpi.oop.game;
 
-public class TimeBomb {
+import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.actions.Invoke;
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class TimeBomb extends AbstractActor {
+    private boolean activated;
+    private float cas;
+    private Animation nonActiv =new Animation("sprites/bomb.png",16,16,0);
+    private Animation Activ =new Animation("sprites/bomb_activated.png",16,16,0);
+    private Animation expl =new Animation("sprites/small_explosion.png",16,16,0);
+
+    public TimeBomb(float sec){
+        setAnimation(nonActiv);
+        activated = false;
+        cas = sec;
+    }
+    public void activate(){
+        activated = true;
+        setAnimation(Activ);
+    }
+
+    public boolean isActivated(){
+        if(activated){
+            return true;
+        }
+        else return  false;
+    }
+    public void addedToScene(Scene scene, Invoke invoke, Reactor defLight){
+        new Loop<>(new Invoke(this::detonate)).scheduleOn(this);
+    }
+
+    private void detonate(){
+        if(activated){
+            setAnimation(Activ);
+            cas--;
+        }
+        else if(cas==0){
+            setAnimation(expl);
+                getScene().removeActor(this);
+        }
+    }
+
 }
