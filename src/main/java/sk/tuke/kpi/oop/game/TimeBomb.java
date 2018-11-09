1896583af5185c29b2a8a89b20f90acff03e8c61
Kristian Paluch <kristian.paluch@student.tuke.sk>
2018-11-09 18:04:21 +0100

    dsad

--- src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
@@ -1,11 +1,13 @@
 package sk.tuke.kpi.oop.game;
 
-import org.jetbrains.annotations.NotNull;
-import sk.tuke.kpi.gamelib.Scene;
-import sk.tuke.kpi.gamelib.actions.Invoke;
+//import org.jetbrains.annotations.NotNull;
+//import sk.tuke.kpi.gamelib.Scene;
+//import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
-import sk.tuke.kpi.gamelib.framework.actions.Loop;
+//import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
+
+import java.util.Objects;
 
 
 public class TimeBomb extends AbstractActor {
@@ -31,7 +33,7 @@ public class TimeBomb extends AbstractActor {
     }
 
     public void reaction() {
-        if (isActivated() && cas > 50) {
+        /*if (isActivated() && cas > 50) {
             setAnimation(Activ);
             cas--;
         } else if (cas <= 50) {
@@ -39,14 +41,32 @@ public class TimeBomb extends AbstractActor {
             cas--;
             if (cas == 0)
                 getScene().removeActor(this);
-        }
+        }*/
+        setAnimation(expl);
+
+        //1000ms na animaciu?
+        Objects.requireNonNull(getScene()).removeActor(this);
+
     }
 
-    @Override
+    /*@Override
     public void addedToScene(@NotNull Scene scene) {
         new Loop<>(new Invoke(this::reaction)).scheduleOn(this);
         super.addedToScene(scene);
+    }*/
+
+    private void timeOut(float seconds) {
+
+        long sec = (long) seconds;
+
+        long startTime = System.currentTimeMillis();
+        for (int count = 0; ;count++) {
+            long now = System.currentTimeMillis();
+            if(now - startTime >= sec*1000) break;
+        }
+        reaction();
     }
+
 
     public float getTime() {
         return cas;
