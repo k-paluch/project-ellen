d48e9c3fcc357a1f2fa3e1c97509d9490564c4f9
Kristian Paluch <kristian.paluch@student.tuke.sk>
2018-12-15 22:01:38 +0100

    kapacita

--- src/main/java/sk/tuke/kpi/oop/game/items/Backpack.java
+++ src/main/java/sk/tuke/kpi/oop/game/items/Backpack.java
@@ -30,7 +30,7 @@ public class Backpack implements ActorContainer<Collectible> {
     @NotNull
     @Override
     public List<Collectible> getContent() {
-        return this.content.subList(0, (getSize()));
+        return this.content.subList(0, kapacita);
     }
 
     @Override
