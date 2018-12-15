df3b53ed3d7af9d7f745f65e029b700c5a313605
Kristian Paluch <kristian.paluch@student.tuke.sk>
2018-12-16 00:05:44 +0100

    dsa

--- src/main/java/sk/tuke/kpi/oop/game/items/Ammo.java
+++ src/main/java/sk/tuke/kpi/oop/game/items/Ammo.java
@@ -2,19 +2,20 @@ package sk.tuke.kpi.oop.game.items;
 
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
-import sk.tuke.kpi.oop.game.characters.Armed;
 import sk.tuke.kpi.oop.game.characters.Ripley;
 
-public class Ammo<T extends Armed> extends AbstractActor implements Usable<Ripley>{
+public class Ammo extends AbstractActor implements Usable<Ripley>{
 
     public Ammo() {
         super("ammo");
         setAnimation(new Animation("sprites/ammo.png", 16, 16));
     }
 
+
+
     @Override
-    public Class<Armed> getUsingActorClass() {
-        return Armed.class;
+    public Class<Ripley> getUsingActorClass() {
+        return Ripley.class;
     }
 
     @Override
