131ec37a6a442c4ea8f13b60d37b8da6a562d1de
Kristian Paluch <kristian.paluch@student.tuke.sk>
2018-12-16 16:54:45 +0100

    sdas

--- src/main/java/sk/tuke/kpi/oop/game/characters/AlienMother.java
+++ src/main/java/sk/tuke/kpi/oop/game/characters/AlienMother.java
@@ -1,13 +1,11 @@
 package sk.tuke.kpi.oop.game.characters;
 
 import sk.tuke.kpi.gamelib.graphics.Animation;
-import sk.tuke.kpi.oop.game.behaviours.Behaviour;
 import sk.tuke.kpi.oop.game.behaviours.RandomlyMoving;
 
 public class AlienMother extends Alien {
 
     private Health health;
-    private Behaviour<? super Alien> behaviour;
     public AlienMother(int healthv){
         super(1000, new RandomlyMoving());
         setAnimation(new Animation("sprites/alien.png", 32, 32, 0.1F, Animation.PlayMode.LOOP_PINGPONG));
